3f403d9eb7178822ec11b38b57ab65e9ccce138b
MariaVanatoa <maria.h.vanatoa@gmail.com>
2017-12-05 11:21:48 +0200

    5.12
    5.12 baltic

--- Assets/Scripts/GameItem.cs
+++ Assets/Scripts/GameItem.cs
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-﻿using System;
-=======
-﻿using System.Collections;
->>>>>>> bb0d65bb48dd24d80f238e33e229bedeb0d67360
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-<<<<<<< HEAD
 public class GameItem
 {
-	private String itemName;
+	private string itemName;
 	/// <summary>
 	/// Initializes a new instance of the <see cref="GameItem"/> class.
 	/// </summary>
 	/// <param name="itemName">Item name.</param>
-	public GameItem (String itemName)
+	public GameItem (string itemName)
 	{
 		this.itemName = itemName;
 	}
@@ -23,24 +18,8 @@ public class GameItem
 	/// Games the name of the item.
 	/// </summary>
 	/// <returns>The item name.</returns>
-	public String GameItemName() {
+	public string GameItemName() {
 		return this.itemName;
 
 	}
 }
-=======
-public class GameItem 
-{
-	private string itemName;
-
-	public GameItem (string itemName)
-		{
-			this.itemName = itemName;
-		}
-
-	public string GameItemName() {
-			return this.itemName;
-
-		}
-	}
->>>>>>> bb0d65bb48dd24d80f238e33e229bedeb0d67360
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class Player
-{
-	private List<GameItem> inventory = new List<GameItem>();
-
-	public Player ()
-	{
-
-	}
-	//TODO: AddItem method, GetItems method
-	/// <summary>
-	/// Adds the item.
-	/// </summary>
-	/// <param name="item">Item.</param>
-	public void AddItem(GameItem item) {
-		inventory.Add (item);
-	}
-	/// <summary>
-	/// Gets the items.
-	/// </summary>
-	/// <returns>The items.</returns>
-	public String GetItems(){
-		string temp = "";
-		foreach (GameItem gameIt in inventory) {
-			temp += gameIt.GameItemName () + ", ";
-		}
-		return temp;
-	}
-	/// <summary>
-	/// Shows the inv.
-	/// </summary>
-	public void ShowInv(){
-		string temp = "";
-		foreach (GameItem gameIt in inventory) {
-			temp += gameIt.GameItemName () + ", ";
-		}
-		Debug.Log (temp);
-	}
-
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -77,4 +31,3 @@ public class Player
 		}
 
 	}
->>>>>>> bb0d65bb48dd24d80f238e33e229bedeb0d67360
